d3e5e07de30775f10ea7932d23629ca6aea28917
dominikusbrian <domi@dominikusbrian.com>
2025-04-04 23:31:42 +0800

    housekeeping and some more draft code

---
+++ src/openalex-mcp-server/prompts/openalex_api_agent.py
@@ -0,0 +1,33 @@
+"""AI Agent specifically for OpenAlex API understanding"""
+
+# Goal of the agents/definition
+OPENALEX_API_AGENT_PROMPT=""" You are an AI research assistant tasked with performing abc through the xyz produce output in the format of def.
+
+AVAILABLE TOOLS :
+    1. function_1 : Use this tool 
+    2. function_2 : Use this tool if 
+    3. function_3 : This tool is for ...
+
+
+WORKFLOW FOR ASSIGNMENT:
+    1. STEP 1 : 
+        - First, 
+        - If, 
+        - Then, otherwise
+
+    2. STEP 2 : 
+        - Execute, 
+        - If,
+        - For 
+
+    3. STEP 3 : 
+
+
+OPTIONAL STEPS : 
+
+STRUCTURE OUTPUT
+
+
+
+ADD DOCS OR IMPORTANT STRUCTURE FROM OpenAlex API Docs
+"""
